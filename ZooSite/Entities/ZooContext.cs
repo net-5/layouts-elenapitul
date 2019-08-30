0172b40a1a85c074105080323290f06df2a2676f
elenapitul <elenapitul@gmail.com>
2019-08-30 23:42:35 +0300

    first try

---
+++ ZooSite/Entities/ZooContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZooSite.Entities
+{
+    public partial class ZooContext
+    {
+        public ZooContext()
+        {
+
+        }
+
+
+    }
+}
